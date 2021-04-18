dec07da2c47b28e2c77ecb84e50a819c595666dd
NoahDrisort <xojziyfay@gmail.com>
2021-04-18 15:14:23 +0000

    update for running with script shell

--- run.sh
+++ run.sh
@@ -24,57 +24,57 @@ then
 ##
 if [[( $3 == "rnn")]];
 then
-python Sentiment_analysis/Glove/{1}.py rnn
+python Sentiment_analysis/Glove/${1}.py rnn
 fi
 
 ##
 if [[( $3 == "lstm")]];
 then
-python Sentiment_analysis/Glove/{1}.py lstm
+python Sentiment_analysis/Glove/${1}.py lstm
 fi
 
 ##
 if [[( $3 == "cnn")]];
 then
-python Sentiment_analysis/Glove/{1}.py cnn 
+python Sentiment_analysis/Glove/${1}.py cnn 
 fi
 
 ##
 if [[( $3 == "gru")]];
 then
-python Sentiment_analysis/Glove/{1}.py gru
+python Sentiment_analysis/Glove/${1}.py gru
 fi
 
 
 ##
 if [[( $3 == "att_dot")]];
 then
-python Sentiment_analysis/Glove/{1}.py att_dot
+python Sentiment_analysis/Glove/${1}.py att_dot
 fi
 
 
 ##
 if [[( $3 == "att_mul")]];
 then
-python Sentiment_analysis/Glove/{1}.py att_mul
+python Sentiment_analysis/Glove/${1}.py att_mul
 fi
 
 ##
 if [[( $3 == "att_add")]];
 then
-python Sentiment_analysis/Glove/{1}.py att_add
+python Sentiment_analysis/Glove/${1}.py att_add
 fi
 
 ##
 if [[( $3 == "bert")]];
 then
-python Sentiment_analysis/Glove/{1}.py bert
+python Sentiment_analysis/Glove/${1}.py bert
 fi
 
 ##
 if [[( $3 == "att_bilstm_add")]];
 then
-python Sentiment_analysis/Glove/{1}.py att_bilstm_add
+python Sentiment_analysis/Glove/${1}.py att_bilstm_add
 fi
 
 fi
